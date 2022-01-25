ee0a5dcec88a208acc8b0f984a4089bef9723f49
Sven Tschoepe <sven@svtsc.de>
2022-01-25 23:42:33 +0100

    Basic functionality

--- maker.py
+++ maker.py
@@ -1,3 +1,29 @@
 import random
 
-print(random.randrange(1,100000))
+words = []
+i = 0
+j = 0
+
+with open('src/wordlist.txt') as my_file:
+    for line in my_file:
+        words.append(line)
+        i = i+1
+
+with open('src/de-2048-v1.txt') as my_file:
+    for line in my_file:
+        words.append(line)
+        i = i+1
+
+#print(i,"words read")
+length = input("How many words do we want? (Zahl)  ")
+
+output = ""
+
+while j < int(length):
+    rnd = random.randrange(0,i)
+    #print(words[rnd])
+    #output = output + words[rnd]
+    output = ''.join((output, words[rnd]))
+    j = j+1
+
+print(output.replace('\n', ' '))
