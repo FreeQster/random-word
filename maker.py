69035b919e2c47cec7e48a74d7d980736bd1a54e
Sven Tschoepe <sven@svtsc.de>
2022-01-26 23:26:47 +0100

    Make script-path relative

--- maker.py
+++ maker.py
@@ -1,29 +1,36 @@
+#Import
 import random
+import os
 
+
+#Define vars
 words = []
 i = 0
 j = 0
 
-with open('src/wordlist.txt') as my_file:
+scd = (os.path.dirname(__file__))
+
+#English wordlist to array
+with open(scd+'/src/wordlist.txt') as my_file:
+    for line in my_file:
+        words.append(line)
+        i = i+1
+#German wordlist to array
+with open(scd+'/src/de-2048-v1.txt') as my_file:
     for line in my_file:
         words.append(line)
         i = i+1
 
-with open('src/de-2048-v1.txt') as my_file:
-    for line in my_file:
-        words.append(line)
-        i = i+1
-
-#print(i,"words read")
-length = input("How many words do we want? (Zahl)  ")
+#Ask for number of words input
+length = input("How many words do we want? (Number)  ")
 
 output = ""
 
+#Roll words and add to var output
 while j < int(length):
     rnd = random.randrange(0,i)
-    #print(words[rnd])
-    #output = output + words[rnd]
     output = ''.join((output, words[rnd]))
     j = j+1
 
+#Output without line breaks
 print(output.replace('\n', ' '))
